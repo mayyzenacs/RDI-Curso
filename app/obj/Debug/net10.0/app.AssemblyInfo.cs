5caf745e3360bc6ce55a450606753532dadbed84
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2026-01-12 20:07:44 -0300

    add: gitignore

--- app/obj/Debug/net10.0/app.AssemblyInfo.cs
+++ app/obj/Debug/net10.0/app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a14a66d5c22c408e481a848b1331985d8b12bf5")]
 [assembly: System.Reflection.AssemblyProductAttribute("app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
