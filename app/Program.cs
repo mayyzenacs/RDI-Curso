2a14a66d5c22c408e481a848b1331985d8b12bf5
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2026-01-12 20:05:02 -0300

    initial commit

---
+++ app/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+      int a = 1;
+      int b = 2;
+
+      for (int i = 1; i < 11; i++)   
+        {
+            if (i == 1 || i == 2)
+            {
+                Console.WriteLine($"Tabuada {a}x{i} = {i*a}"); 
+            }
+            
+            if (i == 10)
+            {
+                Console.WriteLine("---");
+                Console.WriteLine($"Tabuada {a}x{i} = {i*a}"); 
+            }
+        }            
+        Console.WriteLine("");
+        for (int y = 1; y < 3; y++) 
+        {
+            Console.WriteLine($"Tabuada {b}x{y} = {y*b}");    
+        }
+        Console.WriteLine("---");
+        for (int z = 10; z != 0; z--) 
+        {
+            if (z == 10)
+            {
+                Console.WriteLine($"Tabuada {b}x{z} = {z*b}");
+            }
+                
+        }
+    }
+}
