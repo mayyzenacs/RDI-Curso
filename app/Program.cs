e878768b3762149e9b8c8881b06a5c48ba0483d9
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2026-01-20 22:09:49 -0300

    new exercises

--- app/Program.cs
+++ app/Program.cs
@@ -1,15 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
+using app.systemclass;
+
+namespace app;
 
 class Program
 {
-    
-    internal class Pessoa
+    public static void Main()
     {
-        public Pessoa(string nome, int idade)
+        
+        if (Validador.EhMaiordeIdade(18))
         {
-            
+            System.Console.WriteLine("maior de idade");
+        } else
+        {
+             System.Console.WriteLine("Menor de idade");
         }
+        
+        if (Validador.EmailValido("mayraclarolive.com"))
+        {
+            System.Console.WriteLine("email valido");
+        } else
+        {
+            System.Console.WriteLine("Email INVALIDO");
+        }
+
+
+        if (Pedido.ValorEhValido(100))
+        {
+            var pedido = new Pedido(100);
+        } else
+        {
+            System.Console.WriteLine("invalido");
+        }
+
+
+        var sessao1 = new SessaoUsuario("Ana");
+        Console.WriteLine(SessaoUsuario.UsuarioAtivo);
+
+        var sessao2 = new SessaoUsuario("LUCAS");
+        Console.WriteLine(SessaoUsuario.UsuarioAtivo);
     }
-    
 }
